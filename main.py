fe30a1b1227b5abdb48cc4bcd016c1a823635d75
Nicolay237 <68221259+Nicolay237@users.noreply.github.com>
2022-12-15 22:34:44 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,89 +1,98 @@
 import pygame
-from pygame.color import THECOLORS
+import pygame as pg
+import sys
+import os
+import random
+
+pygame.init()
+WIDTH = 1700
+HEIGHT = 900
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+sc = screen
+
+circle_lst = []
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
 
 
 class Board:
     def __init__(self, width, height):
-        pygame.init()
         self.width = width
         self.height = height
         self.board = [[0] * width for _ in range(height)]
-        self.left = 10
-        self.top = 10
+        self.left = 0
+        self.top = 0
         self.cell_size = 32
-        self.lst = []
 
     def set_view(self, left, top, cell_size):
         self.left = left
         self.top = top
         self.cell_size = cell_size
+
+    def render(self, src):
+        flag = True
+        pygame.draw.rect(src,
+                         (0, 255, 127), ((0 + self.left, 0 + self.top),
+                                         (self.width * self.cell_size, self.height * self.cell_size)), 0)
         for i in range(self.height):
             for j in range(self.width):
-                pygame.draw.rect(screen, (46, 139, 87), (self.left, self.top, self.cell_size, self.cell_size), 1)
-                if i == 0 and j == 0:
-                    self.lst.append(self.left)
-                    self.lst.append(self.top)
-                self.left += cell_size
-                if i == self.height - 1 and j == self.width - 1:
-                    self.lst.append(self.left)
-                    self.lst.append(self.top + self.cell_size)
-                    print(*self.lst)
-            self.top += cell_size
-            self.left = left
+                if self.board[i][j] == 1:
+                    image = load_image('test_tree.png')
+                    sc.blit(image, (32 * (j - 1), 32 * (i - 1)))
+                    flag = False
 
-    def render(self, screen):
-        for i in range(self.height):
-            for j in range(self.width):
-                pygame.draw.rect(screen, (46, 139, 87), (self.left, self.top, self.cell_size, self.cell_size), 1)
-                if i == 0 and j == 0:
-                    self.lst.append(self.left)
-                    self.lst.append(self.top)
-                self.left += self.cell_size
-                if i == self.height - 1 and j == self.width - 1:
-                    self.lst.append(self.left)
-                    self.lst.append(self.top + self.cell_size)
+                elif self.board[i][j] == 2:
+                    color = (128, 128, 0)
+                    line_width = 0
+                else:
+                    color = (0, 255, 127)
+                    line_width = 0
+                if flag:
+                    pygame.draw.rect(src,
+                                     color,
+                                     ((j * self.cell_size + self.left, i * self.cell_size + self.top),
+                                     (self.cell_size, self.cell_size)),
+                                     line_width)
 
-            self.top += self.cell_size
-            self.left = 10
+    def on_click(self, pos):
+        x, y = pos
+        x = (x - self.left) // self.cell_size
+        y = (y - self.top) // self.cell_size
+        if 0 <= x < self.width and 0 <= y < self.height:
+            self.board[y][x] += 1
+            self.board[y][x] %= 3
+            print(x, y)
 
-    def get_cell(self, mouse_pos):
-        mouse_pos2 = list(mouse_pos)
-        if (int(mouse_pos2[0]) < self.lst[0] or int(mouse_pos2[1] < self.lst[1])) or \
-                (int(mouse_pos2[0]) > int(self.lst[2]) or int(mouse_pos2[1]) > int(self.lst[3])):
-            print(None)
-            return None
-        else:
-            print(mouse_pos)
-            return mouse_pos
-
-    def on_click(self, cell_coords):
+    def random_spawn_trees(self, sc):
         pass
-#           for j in range(self.width):
-#                self.clc = list(cell_coords)
-#                if self.clc[0] <= self.left or self.clc[1] <= self.top:
-#                    print(i, j)
-#                self.left += self.cell_size
-#            self.top += self.cell_size
-#            self.left = 10
-
-    def get_click(self, mouse_pos):
-        cell = self.get_cell(mouse_pos)
-        self.on_click(cell)
 
 
-if __name__ == '__main__':
-    size = w, h = 1820, 980
-    screen = pygame.display.set_mode(size, pygame.RESIZABLE)
-    screen.fill(THECOLORS['mediumseagreen'])
-    running = True
-    board = Board(56, 30)
+board = Board(53, 28)
+running = True
+while running:
+    sc.fill((0, 0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEMOTION:
+            pass
+        elif event.type == pygame.MOUSEBUTTONUP:
+            pass
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            board.on_click(event.pos)
+        elif event.type == pygame.KEYDOWN:
+            pass
+        elif event.type == pygame.KEYUP:
+            pass
+    screen.fill((0, 0, 0))
     board.render(screen)
 
-    pygame.display.flip()
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                board.get_click(event.pos)
+    pg.display.update()
