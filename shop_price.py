75dc479e4fee7a0b96ca238916007e9c70f7d125
Nicolay237 <iordanovn21@gmail.com>
2023-02-25 13:21:45 +0300

    ъуь

--- shop_price.py
+++ shop_price.py
@@ -1,12 +1,28 @@
 from settings import *
 #{"name": ", "description": ", "price": [], 'image': pygame.image.load(''), "aimage": pygame.image.load(''), "result": pygame.image.load(''), "count": ""}
 lst = [
-    {"name": "clay", "description": "Глиномесы глиномесы", "price": [0, 0, 100, 0], 'image': pygame.image.load('shop\\clay.png'), "aimage": pygame.image.load('shop\\a_clay.png'), "result": pygame.transform.scale(pygame.image.load('shop\\coin.png'), (64, 64)), "count": "5"},
-    {"name": "stone", "description": "Ты причастен к Масонам?", "price": [0, 100, 0, 0], 'image': pygame.image.load('shop\\rock.png'), "aimage": pygame.image.load('shop\\a_rock.png'), "result": pygame.transform.scale(pygame.image.load('shop\\coin.png'), (64, 64)), "count": "5"},
-    {"name": "wood", "description": "Не впервой лес продавать?", "price": [100, 0, 0, 0], 'image': pygame.image.load('shop\\oak.png'), "aimage": pygame.image.load('shop\\a_oak.png'), "result": pygame.transform.scale(pygame.image.load('shop\\coin.png'), (64, 64)), "count": "5"},
-    {"name": "boost", "description": "Брат, че за подкрадули?!", "price": [300, 300, 300, 20], 'image': pygame.image.load('shop\\speed.png'), "aimage": pygame.image.load('shop\\a_speed.png'), "result": pygame.image.load('shop\\speed.png'), "count": "1"},
-    {"name": "barup_lvl1", "description": "Хочешь стать бизнесменом?", "price": [300, 1000, 100, 20], 'image': pygame.image.load('shop\\bar1.png'), "aimage": pygame.image.load('shop\\a_bar1.png'), "result": pygame.image.load('shop\\bar1.png'), "count": "1"},
-    {"name": "barup_lvl2", "description": "Хочешь стать бизнесменом?", "price": [300, 1000, 1000, 100], 'image': pygame.image.load('shop\\bar2.png'), "aimage": pygame.image.load('shop\\a_bar2.png'), "result": pygame.image.load('shop\\bar2.png'), "count": "1"},
-    {"name": "barup_lvl3", "description": "Хочешь стать бизнесменом?", "price": [1000, 1000, 1000, 200], 'image': pygame.image.load('shop\\bar3.png'), "aimage": pygame.image.load('shop\\a_bar3.png'), "result": pygame.image.load('shop\\bar3.png'), "count": "1"},
-    {"name": "barup_lvl4", "description": "Ты бизнесмен!", "price": ["MAX", "MAX", "MAX", "MAX"], 'image': pygame.image.load('shop\\bar4.png'), "aimage": pygame.image.load('shop\\a_bar4.png'), "result": pygame.image.load('shop\\bar4.png'), "count": "1"}
+    {"name": "clay", "description": "Глиномесы глиномесы", "price": [0, 0, 100, 0], 'image': pygame.image.load(
+        'shop\\clay.png'), "aimage": pygame.image.load('shop\\a_clay.png'), "result": pygame.transform.scale(pygame.image.load(
+        'shop\\coin.png'), (64, 64)), "count": "5"},
+    {"name": "stone", "description": "Ты причастен к Масонам?", "price": [0, 100, 0, 0], 'image': pygame.image.load(
+        'shop\\rock.png'), "aimage": pygame.image.load('shop\\a_rock.png'), "result": pygame.transform.scale(pygame.image.load(
+        'shop\\coin.png'), (64, 64)), "count": "5"},
+    {"name": "wood", "description": "Не впервой лес продавать?", "price": [100, 0, 0, 0], 'image': pygame.image.load(
+        'shop\\oak.png'), "aimage": pygame.image.load('shop\\a_oak.png'), "result": pygame.transform.scale(pygame.image.load(
+        'shop\\coin.png'), (64, 64)), "count": "5"},
+    {"name": "boost", "description": "Брат, че за подкрадули?!", "price": [300, 300, 300, 20], 'image': pygame.image.load(
+        'shop\\speed.png'), "aimage": pygame.image.load('shop\\a_speed.png'), "result": pygame.image.load(
+        'shop\\speed.png'), "count": "1"},
+    {"name": "barup_lvl1", "description": "Хочешь стать бизнесменом?", "price": [300, 1000, 100, 20], 'image': pygame.image.load(
+        'shop\\bar1.png'), "aimage": pygame.image.load('shop\\a_bar1.png'), "result": pygame.image.load(
+        'shop\\bar1.png'), "count": "1"},
+    {"name": "barup_lvl2", "description": "Хочешь стать бизнесменом?", "price": [300, 1000, 1000, 100], 'image': pygame.image.load(
+        'shop\\bar2.png'), "aimage": pygame.image.load('shop\\a_bar2.png'), "result": pygame.image.load(
+        'shop\\bar2.png'), "count": "1"},
+    {"name": "barup_lvl3", "description": "Хочешь стать бизнесменом?", "price": [1000, 1000, 1000, 200], 'image': pygame.image.load(
+        'shop\\bar3.png'), "aimage": pygame.image.load('shop\\a_bar3.png'), "result": pygame.image.load(
+        'shop\\bar3.png'), "count": "1"},
+    {"name": "barup_lvl4", "description": "Ты бизнесмен!", "price": ["MAX", "MAX", "MAX", "MAX"], 'image': pygame.image.load(
+        'shop\\bar4.png'), "aimage": pygame.image.load('shop\\a_bar4.png'), "result": pygame.image.load(
+        'shop\\bar4.png'), "count": "1"}
 ]
